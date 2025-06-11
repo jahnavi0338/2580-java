016e5b20c6fa91eadc68c946a9ca1ac27352accd
Gaddiboyina  Sai Jahnavi <sjahnavi1020@gmail.com>
2025-06-11 21:38:58 +0530

    CRM/Interactions.java

---
+++ CRM/Interactions.java
@@ -0,0 +1,21 @@
+package CRM;
+
+public class Interactions {
+    public static void main(String[] args) {
+        String interactionId = "INT2001";
+        String interactionCustomerId = "customerid-45673";
+        String type = "Call";
+        String summary = "Discussed about project requirements";
+        String interactionDate = "09/06/2025";
+        String handledBy = "jaan";
+
+        System.out.println("\n=== Interaction ===");
+        System.out.println("Interaction ID: " + interactionId);
+        System.out.println("Customer ID: " + interactionCustomerId);
+        System.out.println("Type: " + type);
+        System.out.println("Summary: " + summary);
+        System.out.println("interactionDate: " + interactionDate);
+        System.out.println("Handled By: " + handledBy);
+    }
+    
+}
