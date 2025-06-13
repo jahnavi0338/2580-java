53224368efb199658fbb9c30f8fd0c44d71933f6
Gaddiboyina  Sai Jahnavi <sjahnavi1020@gmail.com>
2025-06-13 11:10:53 +0530

    CRM/Bank.java

---
+++ CRM/Bank.java
@@ -0,0 +1,36 @@
+package CRM;
+
+public class Bank {
+    public static void main(String[] args) {
+        String customerName = "Gaddiboyina Sai Jahnavi";
+        String accountNumber = "AB0876543890";
+        int customerIFSCCode = 2341678;
+        double balance = 5000.56;
+        boolean isActive = true;
+        int numberOfTransactions = 2;
+        double depositAmount = 500.0;
+        double withdrawalAmount = 300.0;
+
+        // Perform deposit and withdrawal
+        double totalAmountAfterDeposit = balance + depositAmount;
+        numberOfTransactions++;
+        double totalAmountAfterWithdrawal = totalAmountAfterDeposit - withdrawalAmount;
+         numberOfTransactions++;
+        boolean isMinimumBalance = balance <= 1000;
+
+        boolean isVerified = depositAmount > 0 && isActive;
+
+       
+
+        
+        System.out.println("Account holder name: " + customerName);
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("IFSC Code: " + customerIFSCCode);
+        System.out.println("Minimum balance warning: " + isMinimumBalance);
+        System.out.println("Account is active: " + isActive);
+        System.out.println("Deposit verified: " + isVerified);
+        System.out.println("Final balance: " + totalAmountAfterWithdrawal);
+        System.out.println("Number of transactions: " + numberOfTransactions);
+
+    }
+}
