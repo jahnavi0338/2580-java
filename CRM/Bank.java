d434f45110b3a53c9fbc00f488716a5d6184cc45
Gaddiboyina  Sai Jahnavi <sjahnavi1020@gmail.com>
2025-06-13 11:29:57 +0530

    CRM/Bank.java

--- CRM/Bank.java
+++ CRM/Bank.java
@@ -2,14 +2,14 @@ package CRM;
 
 public class Bank {
     public static void main(String[] args) {
-        String customerName = "Gaddiboyina Sai Jahnavi";
+        String customerName = "G S Jahnavi";
         String accountNumber = "AB0876543890";
         int customerIFSCCode = 2341678;
         double balance = 5000.56;
         boolean isActive = true;
-        int numberOfTransactions = 2;
-        double depositAmount = 500.0;
-        double withdrawalAmount = 300.0;
+        int numberOfTransactions = 5;
+        double depositAmount = 1000.0;
+        double withdrawalAmount = 700.0;
 
         // Perform deposit and withdrawal
         double totalAmountAfterDeposit = balance + depositAmount;
@@ -24,11 +24,11 @@ public class Bank {
 
         
         System.out.println("Account holder name: " + customerName);
-        System.out.println("Account number: " + accountNumber);
-        System.out.println("IFSC Code: " + customerIFSCCode);
-        System.out.println("Minimum balance warning: " + isMinimumBalance);
-        System.out.println("Account is active: " + isActive);
-        System.out.println("Deposit verified: " + isVerified);
+        //System.out.println("Account number: " + accountNumber);
+        //System.out.println("IFSC Code: " + customerIFSCCode);
+        //System.out.println("Minimum balance warning: " + isMinimumBalance);
+        //System.out.println("Account is active: " + isActive);
+        //System.out.println("Deposit verified: " + isVerified);
         System.out.println("Final balance: " + totalAmountAfterWithdrawal);
         System.out.println("Number of transactions: " + numberOfTransactions);
 
